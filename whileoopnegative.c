4d7986e72b524ce74ec391d4b87a3a16c594f035
Sanskar Rathore <114497305+sanskarrathore15@users.noreply.github.com>
2022-10-19 20:15:41 +0530

    Whileloopnegative.c
    Program to take input until use enter negative value.

---
+++ whileoopnegative.c
@@ -0,0 +1,14 @@
+//WAP tp print values till the user enter a -ve value
+#include<stdio.h>
+int main()
+{ 
+  int n;
+    while(n>=0)
+    {
+    
+      printf(" Enter any number ");
+      scanf("%d",&n);
+    }
+    printf("You entered a negative no.");
+return 0;
+}
