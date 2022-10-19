1f3186afcd2a9b53d34a9651ace82a32b3d74491
Jay <97907870+Jay100125@users.noreply.github.com>
2022-10-19 20:08:19 +0530

    Create stringPalindrom.c

---
+++ stringPalindrom.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[] = "madam";
+    char b[6];
+    char i, j;
+    for (i = 0; a[i] != 0; i++)
+    {
+    }
+    i = i - 1;
+    for (j = 0; i >= 0; i--, j++)
+    {
+        b[j] = a[i];
+    }
+    for (i = 0, j = 0; a[i] != '\0' && b[j] != '0'; i++, j++)
+    {
+        if (a[i] != b[j])
+            break;
+    }
+    if (a[i] == b[j])
+    {
+        printf("palindrom");
+    }
+    else
+        printf("string is not palindrom");
+}
