96af9ed8a3c487620402c57720f70abcd81c6192
Shamini27 <Shaminiwanniarachchi27@gmail.com>
2022-10-30 19:19:29 +0530

    Added new palindrome

---
+++ Palindrome/Sample01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int a, n1, rev = 0, rem;
+    
+    printf("Enter any number: ");
+    scanf("%d", &a);    
+    n1 = a;
+    
+    
+    while (a > 0){
+        rem = a % 10;
+        rev = rev * 10 + rem;
+        a = a / 10;
+    }
+    
+    if (n1 == rev){
+        printf("The entered number is a palindromic number\n"); 
+    }
+    else{
+        printf("GThe entered number is not a palindromic number\n"); 
+    }    
+    return 0; 
+}
